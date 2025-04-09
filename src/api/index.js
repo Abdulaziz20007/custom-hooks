0b2007c7abd13f25aaf55ce77b297b6cd479691b
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-04-10 03:45:39 +0500

    done

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const baseURL = "http://152.42.148.49:5000";
 
 export const instance = axios.create({
   baseURL,
